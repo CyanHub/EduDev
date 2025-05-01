5338af16423292443a3a9109d6d4e3175ef33509
Luminus <112999631+CyanHub@users.noreply.github.com>
2025-05-01 22:50:30 +0800

    大规模失误至删除002

--- config/config.go
+++
@@ -1,9 +0,0 @@
-package config
-
-type Server struct {
-	MySQL  MySQL
-	Redis  Redis
-	App    App
-	Jwt    Jwt
-	Logger Logger
-}
--- config/redis.go
+++
@@ -1,8 +0,0 @@
-package config
-
-type Redis struct {
-	Host     string `json:"host"`
-	Port     int    `json:"port"`
-	Password string `json:"password"`
-	DB       int    `json:"db"`
-}
