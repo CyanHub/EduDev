3bb8d8c88e549a9697483e87087222249e51259b
CyanHub <luminusmith@gmail.com>
2025-05-15 17:38:49 +0800

    使用 log.Fatalf 函数记录详细的错误信息，并且终止程序运行。

--- ServerFrameworkDev/GormSerializer/initialize/gorm.go
+++ ServerFrameworkDev/GormSerializer/initialize/gorm.go
@@ -4,10 +4,13 @@ import (
 	"ServerFramework/global"
 	"ServerFramework/model"
 	"fmt"
+	"log"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
+// AutoMigrate 自动迁移数据库表结构
 func AutoMigrate(db *gorm.DB) error {
 	return db.AutoMigrate(
 		&model.User{},
@@ -16,14 +19,15 @@ func AutoMigrate(db *gorm.DB) error {
 		&model.Article{},
 		&model.Subject{},
 	)
-
 }
 
+// MustLoadGorm 初始化 GORM 数据库连接，若失败则终止程序
 func MustLoadGorm() {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?%s", global.CONFIG.MySQL.User, global.CONFIG.MySQL.Password, global.CONFIG.MySQL.Host, global.CONFIG.MySQL.Port, global.CONFIG.MySQL.Database, global.CONFIG.MySQL.Config)
 	db, err := gorm.Open(mysql.Open(dsn))
 	if err != nil {
-		err.Error()
+		// 使用 log.Fatal 记录错误信息并终止程序
+		log.Fatalf("GORM 数据库连接初始化失败: %v", err)
 	}
 	global.DB = db
 }
