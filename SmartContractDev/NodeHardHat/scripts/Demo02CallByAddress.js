8e7d41c570a3efa4f33d4252285e01872ea1e7fe
CyanHub <luminusmith@gmail,com>
2025-05-22 02:37:33 +0800

    添加 转换地址格式 的说明

--- SmartContractDev/NodeHardHat/scripts/Demo02CallByAddress.js
+++ SmartContractDev/NodeHardHat/scripts/Demo02CallByAddress.js
@@ -5,6 +5,8 @@ const { ethers } = require("hardhat")
 async function main() {
 
     //合约地址可从区块链网络终端中复制获取
+    // 地址通常推荐使用 校验和地址（Checksum Address）校验和地址是通过特定算法对地址进行处理，使地址中的部分字符变为大写，这样可以在一定程度上防止地址输入错误
+    // 可以在代码中使用一些库来自动将地址转换为校验和地址。例如，使用 ethereumjs - util 库中的 toChecksumAddress 函数
     //contract address 0xe7f1725e7734ce288f8367e1bb143e90bb3f0512
 
     const Demo02HelloContract = await ethers.getContractAt("Demo02HelloContract", "0xe7f1725e7734ce288f8367e1bb143e90bb3f0512")
