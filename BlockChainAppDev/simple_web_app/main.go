75c5ec9cfa055046e6b11588e28cf41fe5ccf2a7
CyanHub <luminusmith@gmail.com>
2025-05-29 18:00:39 +0800

    汉化Main输出

--- BlockChainAppDev/simple_web_app/main.go
+++ BlockChainAppDev/simple_web_app/main.go
@@ -9,6 +9,6 @@ func main() {
 	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "pong")
 	})
-	fmt.Println("http server start,listen port 8080")
+	fmt.Println("http服务启动,监听端口：8080")
 	http.ListenAndServe(":8080", nil)
 }
