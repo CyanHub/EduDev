45b2e4d413b3dc4480d2c8bb21a215935ed0ea53
CyanHub <luminusmith@gmail.com>
2025-06-02 18:59:11 +0800

    node太多了，也是真服了，后面将在特定环境完成实验

--- SmartContractDev/NodeHardHat/hardhat.config.js
+++ SmartContractDev/NodeHardHat/hardhat.config.js
@@ -5,8 +5,8 @@ module.exports = {
   solidity: "0.8.28",
   networks: {
     localhost: {
-      url: "http://127.0.0.1:8545/",
-      chainId: 31337,
+      url: "http://127.0.0.1:8545", // 本地节点RPC地址
+      chainId: 31337 // Hardhat本地网络默认链ID
     }
   }
 };
